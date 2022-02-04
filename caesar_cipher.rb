b137975d9409c62e916fa24ffadc9213782d764a
Laszlo Kiss <pinewood82@gmail.com>
2022-02-04 13:59:50 +0100

    Add cipher method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,22 @@
-def caesar_cipher(text, number) {
-    
-}
+def caesar_cipher(text, number)
+    text.split('').map(&:ord).map {|code| get_char(code, number)}.join
+    end
+end
+
+def get_char(code, number)
+    number = number % 26
+    new_code = code + number
+    if (97..122).include?(code)
+        new_code = new_code > 122 ? new_code - 26 
+        : new_code < 97 ? new_code + 26 
+        : new_code
+    elsif (65..90).include?(code)
+        new_code = new_code > 90 ? new_code - 26
+        : new_code < 65 ? new_code + 26
+        : new_code
+    else
+        new_code = code
+    end
+    new_code.chr
+end
+
