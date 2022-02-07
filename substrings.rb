b012132de833715f4380e83057805532ea226de2
Laszlo Kiss <pinewood82@gmail.com>
2022-02-07 14:54:15 +0100

    Add variable for downcase substring

--- substrings.rb
+++ substrings.rb
@@ -1,21 +1,23 @@
 #word = ARGV[0].downcase
-#dictionary = ARGV[1].downcase.split
+#dictionary = ARGV[1].split
 
 word = "Howdy partner, sit down! How's it going?"
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","Go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substring(word, dictionary)
     result = Hash.new(0)
     word.downcase!
-    dictionary.each {|w| w.downcase!}.reduce(result) do |result, w|
-        count = word.end_with?(w) ? 
-        word.split(w).length == 0 ? 1
-        : word.split(w).length
-        : word.split(w).length - 1
+    dictionary.reduce(result) do |result, w|
+        wd = w.downcase
+        count = word.end_with?(wd) ? 
+        word.split(wd).length == 0 ? 1
+        : word.split(wd).length
+        : word.split(wd).length - 1
         result[w] += count
         result
     end
     result.select {|k, v| v > 0}
 end
 
-puts substring(word, dictionary)
+puts "If uncomment line 1 and 2, and comment line 4 and 5, it can be run with command line arguments"
+puts "The result is: #{substring(word, dictionary)}"
