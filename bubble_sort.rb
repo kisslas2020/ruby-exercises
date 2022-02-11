88acae831dbfbcb889613824ecf1e129d3c4537a
Laszlo Kiss <pinewood82@gmail.com>
2022-02-11 13:50:09 +0100

    Add solution for bubble sort project

---
+++ bubble_sort.rb
@@ -0,0 +1,31 @@
+unsorted_array = ARGV.empty? ? [4,3,78,2,0,2] : ARGV[0].split(',').map {|n| n.to_i}
+
+def bubble_sort(unsorted_array)
+    count = 1
+    end_index = unsorted_array.length - 2
+    until count == 0
+        count = iterate_round(unsorted_array, end_index)
+        end_index -= 1
+    end
+    unsorted_array
+end
+
+def iterate_round(unsorted_array, end_index)
+    count = 0
+    for i in 0..(end_index)
+        if unsorted_array[i] > unsorted_array[i + 1]
+            unsorted_array = swap(i, unsorted_array)
+            count += 1
+        end
+    end
+    count
+end
+
+def swap(index, unsorted_array)
+    temp = unsorted_array[index]
+    unsorted_array[index] = unsorted_array[index + 1]
+    unsorted_array[index + 1] = temp
+    unsorted_array
+end
+
+p bubble_sort(unsorted_array)
