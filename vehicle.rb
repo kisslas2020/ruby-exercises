940416d81ff108f30a90c4db9e5ea373e562b13c
Laszlo Kiss <pinewood82@gmail.com>
2022-02-17 16:45:58 +0100

    Create superclass Vehicle and move methods from MyCar class

---
+++ vehicle.rb
@@ -0,0 +1,34 @@
+class Vehicle
+    attr_accessor :year, :color, :model, :speed
+    
+    def initialize(year, color, model)
+        @year = year
+        @color = color
+        @model = model
+        @speed = 0
+    end
+
+    def speed_up
+        self.speed += 1
+    end
+
+    def brake
+        self.speed -= 1
+    end
+
+    def shut_off
+        self.speed.times {brake}
+    end
+
+    def spray_paint=(new_color)
+        self.color = new_color
+    end
+
+    def self.mileage(consumption, distance)
+        (1.0 * consumption / distance).round(4) * 100
+    end
+
+    def to_s
+        "This is a car of model #{self.model} from year #{self.year},\nits color is #{self.color} and its current speed is #{self.speed} km/h"
+    end
+end
