0d2df9105639bd5a280e24326347a33486a14559
Laszlo Kiss <pinewood82@gmail.com>
2022-02-17 09:58:56 +0100

    Add solution Exercise 2

--- my_car.rb
+++ my_car.rb
@@ -24,15 +24,21 @@ end
 opel = MyCar.new(1998, "red", "Astra")
 
 puts opel.speed
-
 50.times {opel.speed_up}
-
 puts opel.speed
-
 30.times {opel.brake}
-
 puts opel.speed
-
 opel.shut_off
-
 puts opel.speed
+
+puts "=============================="
+
+puts opel.color
+puts opel.year
+puts opel.model
+opel.year = 2000
+opel.color = "green"
+opel.model = "Corsa"
+puts opel.color
+puts opel.year
+puts opel.model
